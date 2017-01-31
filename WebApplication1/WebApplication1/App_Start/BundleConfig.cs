8bc4a86affa952a53da6cda4c2ea652b46e76296
Tim3Ds <Tim3Ds@gmail.com>
2017-01-31 11:51:59 -0700

    add jquery-ui files

--- WebApplication1/WebApplication1/App_Start/BundleConfig.cs
+++ WebApplication1/WebApplication1/App_Start/BundleConfig.cs
@@ -28,7 +28,13 @@ namespace WebApplication1
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/jquery-ui.css",
+                      "~/Content/jquery-ui.min.css",
+                      "~/Content/jquery-ui.structure.css",
+                      "~/Content/jquery-ui.structure.min.css",
+                      "~/Content/jquery-ui.theme",
+                      "~/Content/jquery-ui.theme.min.css"));
         }
     }
 }
