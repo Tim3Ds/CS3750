e741458653740cf949ae9c9e7d3ae1d73a7ca819
Tim3Ds <Tim3Ds@gmail.com>
2017-01-31 11:23:59 -0700

    add ui bundler

--- WebApplication1/WebApplication1/App_Start/BundleConfig.cs
+++ WebApplication1/WebApplication1/App_Start/BundleConfig.cs
@@ -8,6 +8,9 @@ namespace WebApplication1
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/jquery-ui").Include(
+                        "~/Scripts/jquery-ui{version}.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
